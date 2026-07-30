bcd47fa2beb74e9fc5f3a9aaf56be2288dc0b0bf
ivanisgood52 <142957816+ivanisgoodatcoding52@users.noreply.github.com>
2026-07-30 17:22:11 -0500

    Update dpkg.sh for package downloads and installations

--- dpkg.sh
+++ dpkg.sh
@@ -1,20 +1,21 @@
-# 1. Enable your system's 32-bit architecture support
+#!/bin/bash
+set -e
+
 sudo dpkg --add-architecture i386
 sudo apt update
 
-# 2. Download the 64-bit dependency packages
-curl -O http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.4-2_amd64.deb
-curl -O http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.4-2_amd64.deb
+BASE=https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses
 
-# 3. Download the 32-bit dependency packages
-curl -O http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.4-2_i386.deb
-curl -O http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.4-2_i386.deb
+# amd64
+wget -c $BASE/libtinfo5_6.3-2ubuntu0.2_amd64.deb
+wget -c $BASE/libncurses5_6.3-2ubuntu0.2_amd64.deb
 
-# 4. Install them sequentially
-sudo dpkg -i libtinfo5_6.4-2_amd64.deb
-sudo dpkg -i libncurses5_6.4-2_amd64.deb
-sudo dpkg -i libtinfo5_6.4-2_i386.deb
-sudo dpkg -i libncurses5_6.4-2_i386.deb
+# i386
+wget -c $BASE/libtinfo5_6.3-2ubuntu0.2_i386.deb
+wget -c $BASE/libncurses5_6.3-2ubuntu0.2_i386.deb
 
-# 5. Clean up the downloaded packages
-rm -f libtinfo5_*.deb libncurses5_*.deb
+sudo apt install ./libtinfo5_6.3-2ubuntu0.2_amd64.deb \
+                 ./libncurses5_6.3-2ubuntu0.2_amd64.deb \
+                 ./libtinfo5_6.3-2ubuntu0.2_i386.deb \
+                 ./libncurses5_6.3-2ubuntu0.2_i386.deb
+
