192137104cb4bc80465de0ae81ba8b7c54bd004d
ivanisgood52 <142957816+ivanisgoodatcoding52@users.noreply.github.com>
2026-07-30 16:25:03 -0600

    Install Python 2.7 and create symlink
    Added commands to download and install Python 2.7 from Ubuntu archives, create a symlink for python2, and clean up the downloaded package.

--- dpkg.sh
+++ dpkg.sh
@@ -19,6 +19,12 @@ sudo apt install ./libtinfo5_6.3-2ubuntu0.2_amd64.deb \
                  ./libtinfo5_6.3-2ubuntu0.2_i386.deb \
                  ./libncurses5_6.3-2ubuntu0.2_i386.deb
 
+# Download and install Python 2.7 DEB packages from Ubuntu 22.04 archives
+wget http://security.ubuntu.com/ubuntu/pool/universe/p/python2.7/python2.7-minimal_2.7.18-13ubuntu1.5_amd64.deb
+sudo apt install -y ./python2.7-minimal_2.7.18-13ubuntu1.5_amd64.deb
+sudo ln -sf /usr/bin/python2.7 /usr/local/bin/python2
+rm python2.7-minimal_2.7.18-13ubuntu1.5_amd64.deb
+
 sudo apt install -y git-core gnupg flex bison build-essential zip curl \
   zlib1g-dev libx11-dev libxml2-utils xsltproc unzip openjdk-8-jdk bc rsync \
   libstdc++6:i386 zlib1g:i386 python2-minimal nano
