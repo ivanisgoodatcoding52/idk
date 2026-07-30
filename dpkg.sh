deb4daa4efd8589738db6024bee072b4fdd92217
ivanisgood52 <142957816+ivanisgoodatcoding52@users.noreply.github.com>
2026-07-30 16:38:54 -0600

    Refactor dpkg.sh for Python 2 installation process

--- dpkg.sh
+++ dpkg.sh
@@ -25,23 +25,24 @@ sudo apt install -y build-essential libssl-dev zlib1g-dev \
   libncursesw5-dev xz-utils tk-dev libxml2-dev \
   libxmlsec1-dev libffi-dev liblzma-dev git
 
-mkdir -p ~/python2-debs && cd ~/python2-debs
-# 2. Re-map strings to target the updated security micro-releases
-B_URL="http://ubuntu.com"
-VERSION_SUFX="2.7.18-13ubuntu1.12_amd64.deb"
+mkdir -p ~/python2-master && cd ~/python2-master
 
-# 3. Fetch each interlocking .deb block securely
-wget "${B_URL}/libpython2.7-minimal_${VERSION_SUFX}"
-wget "${B_URL}/python2.7-minimal_${VERSION_SUFX}"
-wget "${B_URL}/libpython2.7-stdlib_${VERSION_SUFX}"
-wget "${B_URL}/python2.7_${VERSION_SUFX}"
+# 2. Map standard directory endpoints to the Main Archive Pool
+M_POOL="http://ubuntu.com"
+V_TAG="2.7.18-13ubuntu1_amd64.deb"
 
-# 4. Force simultaneous localized package configuration
+# 3. Pull down the exact full system package chain sequentially
+wget "${M_POOL}/libpython2.7-minimal_${V_TAG}"
+wget "${M_POOL}/python2.7-minimal_${V_TAG}"
+wget "${M_POOL}/libpython2.7-stdlib_${V_TAG}"
+wget "${M_POOL}/python2.7_${V_TAG}"
+
+# 4. Simultaneously force configure the dependency chain via dpkg
 sudo dpkg -i libpython2.7-minimal_*.deb python2.7-minimal_*.deb libpython2.7-stdlib_*.deb python2.7_*.deb
 
-# 5. Erase installation trace leftovers
-cd ~ && rm -rf ~/python2-debs
+# 5. Clean up downloaded package remnants
+cd ~ && rm -rf ~/python2-master
 
-# 6. Re-bind target execution pointer masks
+# 6. Rebind execution paths for legacy Qualcomm build triggers
 sudo ln -sf /usr/bin/python2.7 /usr/local/bin/python2
 sudo ln -sf /usr/bin/python2.7 /usr/local/bin/python2.7
